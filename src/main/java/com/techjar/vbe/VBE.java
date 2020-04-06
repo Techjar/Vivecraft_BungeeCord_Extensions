f51a177b795bd1a4e361074e8f0e7f691c505428
Techjar <tecknojar@gmail.com>
2020-04-06 05:54:19 -0400

    Initial commit

---
+++ src/main/java/com/techjar/vbe/VBE.java
@@ -0,0 +1,37 @@
+package com.techjar.vbe;
+
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.connection.Server;
+import net.md_5.bungee.api.event.PluginMessageEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.event.EventHandler;
+
+public class VBE extends Plugin implements Listener {
+	
+	final static String CHANNEL = "vivecraft:data";	
+
+	@Override
+	public void onEnable() {
+		this.getProxy().registerChannel(CHANNEL);
+	}
+
+	@Override
+	public void onDisable() {
+		this.getProxy().unregisterChannel(CHANNEL);
+	}
+	
+	@EventHandler
+    public void on(PluginMessageEvent event){
+		if (!event.getTag().equalsIgnoreCase(CHANNEL)) return;
+		if(event.getReceiver() instanceof ProxiedPlayer) {
+			ProxiedPlayer player = (ProxiedPlayer) event.getReceiver();
+			player.sendData(CHANNEL, event.getData());
+		} else if(event.getReceiver() instanceof Server) {
+			Server server = (Server) event.getReceiver();
+			server.sendData(CHANNEL, event.getData());
+		}        
+    }
+
+
+}
