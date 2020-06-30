b734fa4d809889f6c704550fed370b7ff26ce53b
Techjar <tecknojar@gmail.com>
2020-06-30 19:21:24 -0400

    Support older Vivecraft versions too

--- src/main/java/com/techjar/vbe/VBE.java
+++ src/main/java/com/techjar/vbe/VBE.java
@@ -7,32 +7,34 @@ import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
 
-public class VBE extends Plugin implements Listener {
+import java.util.Arrays;
 
-	public static final String CHANNEL = "vivecraft:data";
+public class VBE extends Plugin implements Listener {
+	public static final String[] CHANNELS = new String[]{"vivecraft:data", "Vivecraft"};
 
 	@Override
 	public void onEnable() {
-		this.getProxy().registerChannel(CHANNEL);
+		for (String channel : CHANNELS)
+			this.getProxy().registerChannel(channel);
 	}
 
 	@Override
 	public void onDisable() {
-		this.getProxy().unregisterChannel(CHANNEL);
+		for (String channel : CHANNELS)
+			this.getProxy().unregisterChannel(channel);
 	}
 
 	@EventHandler
 	public void on(PluginMessageEvent event) {
-		if (!event.getTag().equalsIgnoreCase(CHANNEL))
+		String channel = event.getTag();
+		if (Arrays.stream(CHANNELS).noneMatch(channel::equals))
 			return;
 		if (event.getReceiver() instanceof ProxiedPlayer) {
 			ProxiedPlayer player = (ProxiedPlayer)event.getReceiver();
-			player.sendData(CHANNEL, event.getData());
+			player.sendData(channel, event.getData());
 		} else if (event.getReceiver() instanceof Server) {
 			Server server = (Server)event.getReceiver();
-			server.sendData(CHANNEL, event.getData());
+			server.sendData(channel, event.getData());
 		}
 	}
-
-
 }
